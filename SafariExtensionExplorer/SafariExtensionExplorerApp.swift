c25e24b7bcd32bff8767848cb87cdf10431e592a
carlynorama <carlynorama@gmail.com>
2024-01-29 10:57:40 -0800

    Initial Commit

---
+++ SafariExtensionExplorer/SafariExtensionExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariExtensionExplorerApp.swift
+//  SafariExtensionExplorer
+//
+//  Created by Carlyn Maw on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SafariExtensionExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
